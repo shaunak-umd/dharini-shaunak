b7ef0fc31605862a2626b06b7dae5e12d936e8f7
Shaunak Bhanarkar <shaunak.bhanarkar@gmail.com>
2022-10-26 17:00:15 -0400

    test d3 for barchart

--- src/Components/BarChart.jsx
+++ src/Components/BarChart.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import * as d3 from "d3";
+import { useD3Hook } from "../Hooks/useD3Hook";
 
 
 const BarChart = ({harry_potter_data}) =>{
 
     console.log(harry_potter_data);
 
+    
+    
+    
+    const alphabet = [
+      {letter: 'a', frequency:1},
+      {letter: 'b', frequency:5},
+      {letter: 'c', frequency:6},
+      {letter: 'd', frequency:2},
+      {letter: 'e', frequency:4}
+    ];
+
+    
+    
     // Copyright 2021 Observable, Inc.
 // Released under the ISC license.
 // https://observablehq.com/@d3/bar-chart
-const d3BarChart = (data, {
+const d3BarChart = (data=alphabet, {
     x = (d, i) => i, // given d in data, returns the (ordinal) x-value
     y = d => d, // given d in data, returns the (quantitative) y-value
     title, // given d in data, returns the title text
@@ -93,15 +107,48 @@ const d3BarChart = (data, {
     svg.append("g")
         .attr("transform", `translate(0,${height - marginBottom})`)
         .call(xAxis);
-  
+
+    // console.log('svg is here');
+    // console.log(svg);
+    // const abc = svg.html();
+    // const pqr = <div />
+    // return <div>{abc}</div>;
     return svg.node();
   }
-  
 
+  const chart = d3BarChart(alphabet, {
+    x: d => d.letter,
+    y: d => d.frequency,
+    xDomain: d3.groupSort(alphabet, ([d]) => -d.frequency, d => d.letter), // sort by descending frequency
+    yFormat: "%",
+    yLabel: "↑ Frequency",
+    width: 500,
+    height: 500,
+    color: "steelblue"
+  });
+
+  // const ref = useD3Hook(d3BarChart, 300);
+  
+  // console.log(chart);
+  // let ref = React.useRef();
+  
     return(
         <div>
-        barchart
-
+        {/* <svg dangerouslySetInnerHTML={{__html: chart}} /> */}
+        {/* <div ref={node => node.appendChild(chart)}></div> */}
+        {/* <svg
+            ref={ref}
+          style={{
+            height: 500,
+            width: "100%",
+            marginRight: "0px",
+            marginLeft: "0px",
+          }}
+        >
+          <g className="plot-area" />
+          <g className="x-axis" />
+          <g className="y-axis" />
+        </svg> */}
         </div>
     )
 }
